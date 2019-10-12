b35bc61606274cbbbe30bc46cde815b970dd0274
Rodolfo do Nascimento Azevedo <rof20004@gmail.com>
2019-10-12 16:58:56 -0400

    Lambda functions call testing

---
+++ functions/create_card.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	if request.HTTPMethod != "POST" {
+		return nil, errors.New("Method not allowed")
+	}
+
+	return &events.APIGatewayProxyResponse{
+		StatusCode: http.StatusOK,
+		Body:       "Hello, World",
+	}, nil
+}
+
+func main() {
+	// Make the handler available for Remote Procedure Call by AWS Lambda
+	lambda.Start(handler)
+}
